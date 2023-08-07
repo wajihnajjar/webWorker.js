a892ce7164a809c93b7237387b5bfee56291a968
wajihnajjar <wajih.najjar@esprit.tn>
2023-08-07 14:23:02 +0100

    Adding Web Workers

--- main.js
+++ main.js
@@ -5,25 +5,13 @@ const  divOfCounter = document.getElementsByClassName("counter")
 divOfCounter[0].innerHTML= (parseInt(divOfCounter[0].innerHTML)+1).toString()
 }
 
-const helper= (n)=>{
-if(n<=0){
-return 0 ; 
-}
-if(n==1 || n==2)
-return 1 
-return helper(n-1)+helper(n-2)
 
-}
-const fib  =(n)=>{
-
-const result = helper(n)
-console.log(result)
-
-
-}
 
 worker.addEventListener("message",(e)=>{
 console.log("worker Said" , e.data)
 },false)
 
-worker.postMessage('Hello World')
+
+const  fibx = (n)=>{
+worker.postMessage(n)
+}
--- worker.js
+++ worker.js
@@ -1,4 +1,20 @@
+
+const helper= (n)=>{
+    if(n<=0){
+    return 0 ; 
+    }
+    if(n==1 || n==2)
+    return 1 
+    return helper(n-1)+helper(n-2)
+    
+    }
+    const fib  =(n)=>{
+    
+    const result = helper(n)
+    return result    
+    
+    }
+
 self.addEventListener("message",(e)=>{
-console.log("Main Said ",e.data)
-self.postMessage("Hello World From Worker")
+    self.postMessage(fib(e.data))
 },false)
